4e1346e57fbde5afdceed9a9cd3566503a373706
holombik-d <dmitriy.holombik@zagravagames.com>
2024-01-20 19:01:55 +0200

    Added clean project

---
+++ Source/Samurai/SamuraiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SamuraiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMURAI_API ASamuraiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
