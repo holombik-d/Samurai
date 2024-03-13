d991200bc0a725bf76db1981089d874e50f80853
holombik-d <dmitriy.holombik@zagravagames.com>
2024-03-13 17:47:58 +0200

    Fixed build compilation

--- Source/Samurai/Samurai.Build.cs
+++ Source/Samurai/Samurai.Build.cs
@@ -8,7 +8,7 @@ public class Samurai : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "AIModule", "GameplayTasks"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
